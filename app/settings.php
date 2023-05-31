3b8a0ca70f3f64b1b4a7fade533fa61e6f9e051c
Daria Vasilega <dariavasilega@gmail.com>
2023-05-31 13:24:43 +0300

    Resolves #5: Project Skeleton Cleanup.
    - Removed unused functionality.
    - Added mechanism, which allows to safety provide credentials (database credentials in this case).
    - Reconfigured .gitignore.

--- app/settings.php
+++ app/settings.php
@@ -13,14 +13,15 @@ return function (ContainerBuilder $containerBuilder) {
     $containerBuilder->addDefinitions([
         SettingsInterface::class => function () {
             return new Settings([
-                'displayErrorDetails' => true, // Should be set to false in production
-                'logError'            => false,
-                'logErrorDetails'     => false,
+                'displayErrorDetails' => true, // TODO: disable 3 lines below in prod mod
+                'logError'            => true,
+                'logErrorDetails'     => true,
                 'logger' => [
                     'name' => 'slim-app',
                     'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
                     'level' => Logger::DEBUG,
                 ],
+                'db' => require __DIR__ . '/credentials/db.php',
             ]);
         }
     ]);
