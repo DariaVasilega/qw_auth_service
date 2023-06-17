f9d86db91ce24a2847c31be970883de6f39e1109
Daria Vasilega <dariavasilega@gmail.com>
2023-06-17 09:16:41 +0300

    Resolves #5: Database Migrations.
    - Configured Phinx (DB migrations runner).
    - Implemented base migration class.

---
+++ app/phinx-config.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+$dbConfig = require __DIR__ . '/credentials/db.php';
+
+return [
+    'paths' => [
+        'migrations' => 'migrations'
+    ],
+    'migration_base_class' => \App\Migration\Migration::class,
+    'environments' => [
+        'default_migration_table' => 'migration',
+        'default_environment' => 'mainline',
+        'mainline' => [
+            'adapter' => $dbConfig['driver'],
+            'host' => $dbConfig['host'],
+            'port' => $dbConfig['port'],
+            'name' => $dbConfig['database'],
+            'user' => $dbConfig['username'],
+            'pass' => $dbConfig['password'],
+        ],
+    ]
+];
