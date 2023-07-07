ca37f20338bb7a1b9699fbf6844db18c3b9d5c68
Daria Vasilega <dariavasilega@gmail.com>
2023-07-07 17:47:39 +0300

    Resolves #15: Permission Validation.
    - Declared validation rules for permission entity.
    - Added translations.

---
+++ resources/i18n/uk_UA/permission.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+// phpcs:disable Generic.Files.LineLength.TooLong
+
+return [
+    'validation.code.regex' => ':Attribute має складатися лише з літер латинського алфавіту "A-z" і символів "_" або "-".',
+];
