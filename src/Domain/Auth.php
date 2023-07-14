62bdbf610ed224a5fa1aab4550e4c7802d5059b3
Daria Vasilega <dariavasilega@gmail.com>
2023-07-14 22:42:02 +0300

    Resolves #6: Auth Entity.
    - Created `auth` entity. Declared user-auth entities relations.

---
+++ src/Domain/Auth.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain;
+
+/**
+ * @property string $email
+ * @property string $token
+ * @property string $expiration
+ */
+class Auth extends \Illuminate\Database\Eloquent\Model
+{
+    /**
+     * @inheritDoc
+     */
+    protected $table = 'auth';
+
+    /**
+     * @inheritDoc
+     */
+    public $timestamps = false;
+
+    /**
+     * @inheritDoc
+     */
+    public $incrementing = false;
+
+    /**
+     * @inheritDoc
+     */
+    protected $primaryKey = 'email';
+
+    /**
+     * @inheritDoc
+     */
+    protected $fillable = [
+        'email',
+        'token',
+        'expiration',
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function user(): \Illuminate\Database\Eloquent\Relations\HasOne
+    {
+        return $this->hasOne(\App\Domain\User::class, 'email');
+    }
+}
